7c1ef6f92812c19b62763c5b24e0f2afbe3dbd95
sportsmattress <154449583+sportsmattress@users.noreply.github.com>
2024-01-03 20:24:07 -0500

    Add files via upload

---
+++ rock-paper-scissors/index.js
@@ -0,0 +1,47 @@
+const playerText = document.getElementById("playerText");
+const computerText = document.getElementById("computerText");
+const resultText = document.getElementById("resultText");
+const playerScore = document.getElementsByClassName("playerScore");
+const computerScore = document.getElementsByClassName("computerScore");
+choices = ['rock', 'paper', 'scissors']
+let player = 0;
+let computer = 0;
+
+function playGame(playerChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * 3)];
+    let result = '';
+
+    if(playerChoice === computerChoice) {
+        result = 'YOU DRAW!'
+    }
+    else {
+        switch(playerChoice) {
+            case 'rock':
+                result = (computerChoice === 'scissors') ? "You Win!" : "You Lose!";
+                break;
+            case 'scissors':
+                result = (computerChoice === 'paper') ? "You Win!" : "You Lose!";
+                break;
+            case 'paper':
+                result = (computerChoice === 'rock') ? "You Win!" : "You Lose!";
+        }
+    }
+    playerText.textContent = `Player: ${playerChoice}`;
+    computerText.textContent = `Computer: ${computerChoice}`;
+    resultText.textContent = result;
+
+
+    if(result = "You Win!"){
+        player++;
+        playerScore.textContent = `Player Score: ${player}`;
+        }
+    if(result = "You Lose!"){
+        computer++;
+        computerScore.textContent = `Computer Score: ${computer}`;
+        }
+
+    }
+
+
+     
+    
